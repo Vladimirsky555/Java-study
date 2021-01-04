e45dfa2c63e15c60ea5afd18cee936cb9ae3aefd
Vladimirsky <vladimir.bervin@gmail.com>
2021-01-04 19:36:02 +0300

    add lesson 101.  Swing6.  WindowListener

---
+++ tmp.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame jFrame = getFrame();
+        JPanel jPanel = new JPanel();
+        jFrame.add(jPanel);
+    }
+    
+    
+    static JFrame getFrame(){
+        int h = 400; int w = 500;
+        JFrame jFrame = new JFrame(){};
+        jFrame.setVisible(true);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width/2 - w/2,dimension.height/2 - h/2, w,h);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setTitle("My program");
+        return jFrame;
+    }
+}
