3efff36c9e56d612d0eb511aa759c80d9827a3cb
Vladimirsky <vladimir.bervin@gmail.com>
2021-01-09 16:03:35 +0300

    add lesson 116.  Swing21.  Focus

--- src/116_Swing_Focus.java
+++ src/116_Swing_Focus.java
@@ -2,21 +2,24 @@ import javax.swing.*;
 import java.awt.*;
 
 /*
-
+Фокус по умолчанию идёт слева направо, начиная с 1-го элемента
 */
 
 public class Main {
-	
-	    static JFrame jFrame = getFrame();
-        static JPanel jPanel = new JPanel();
-		
+
+    static JFrame jFrame = getFrame();
+    static JPanel jPanel = new JPanel();
+
     public static void main(String[] args) {
         jFrame.add(jPanel);
+        jPanel.add(new JButton("button1"));
+        jPanel.add(new JButton("button2")).setFocusable(false);//никогда не будет в фокусе
+        jPanel.add(new JButton("button3")).requestFocusInWindow();//переназначаем фокус
     }
 
 
     static JFrame getFrame(){
-        int h = 500; int w = 600;
+        int h = 400; int w = 500;
         JFrame jFrame = new JFrame(){};
         jFrame.setVisible(true);
         Toolkit toolkit = Toolkit.getDefaultToolkit();
